d6e7c9c690ab92bdc88ecd8d43d7ae9b750a7ec7
hifichevymane <blekbjek@gmail.com>
2025-04-28 23:10:56 +0300

    12 завдання

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Product(models.Model):
+    title = models.CharField(max_length=250)
+    price = models.FloatField()
+    product_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
