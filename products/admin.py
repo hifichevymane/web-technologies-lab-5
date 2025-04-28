723a731805d2fd24e19388c7e36151756fae8b95
hifichevymane <blekbjek@gmail.com>
2025-04-28 23:22:17 +0300

    виправлене 16 завдання

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from models import Category, Product
+from . import models
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(models.Category)
+admin.site.register(models.Product)
