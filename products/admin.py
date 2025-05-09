7bb6cf139bb0dfc9c524fe47b56e6db1cc08105c
hifichevymane <blekbjek@gmail.com>
2025-05-10 00:02:44 +0300

    advanced admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
-admin.site.register(models.Category)
-admin.site.register(models.Product)
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title']
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'product_qty', 'category', 'updated_at']
+    list_editable = ['price', 'product_qty', 'category']
+    list_filter = ['category', 'updated_at']
+    search_fields = ['title', 'category__title']
