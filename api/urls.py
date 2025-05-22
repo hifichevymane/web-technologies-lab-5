6316e7a16b531551a2187a026614c267cd3c7fac
hifichevymane <blekbjek@gmail.com>
2025-05-22 22:03:52 +0300

    add resources and routes

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from tastypie.api import Api
+from .views import CategoryResource, ProductResource
+
+category_resource = Api(api_name='v1')
+category_resource.register(CategoryResource())
+
+product_resource = Api(api_name='v1')
+product_resource.register(ProductResource())
+
+urlpatterns = [
+    path('', include(category_resource.urls)),
+    path('', include(product_resource.urls))
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from tastypie.resources import ModelResource
+from products.models import Category, Product
 
 # Create your views here.
+class CategoryResource(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resource_name = 'categories',
+        allowed_methods = ['get']
+
+
+class ProductResource(ModelResource):
+    class Meta:
+        queryset = Product.objects.all()
+        resource_name = 'products'
+        allowed_methods = ['get']
